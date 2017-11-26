f18b52e3b66c2c3336d3f6cc8cc831827548c4ff
常燕飞 <swallowfly@changyanfeideMacBook-Pro.local>
2017-11-26 22:03:53 +0800

    1.新建博客项目

---
+++ config/social.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+/**
+ * Created by PhpStorm.
+ * User: lufficc
+ * Date: 2016/8/21
+ * Time: 22:47
+ */
+    'facebook' => [
+        'url'  => 'https://www.facebook.com/swallowfly',
+        'icon'   => 'facebook',
+    ],
+    'twitter'  => [
+        'url'  => 'https://twitter.com/swallowfly',
+        'icon'   => 'twitter',
+    ],
+    'weibo'    => [
+        'url'  => 'http://weibo.com/u/3899773249',
+        'icon'   => 'weibo',
+    ],
+    'github'  => [
+        'url' =>'https://github.com/capitalCodingguy',
+        'icon'   => 'github',
+    ],
+
+];
